cb1ab244a278b6a1b4c2924665e7d115854b5199
Prabhat Prajapati <prabhatprajapati21@gmail.com>
2026-02-24 13:06:06 +0530

    service page final update

--- src/app/services/quality-and-compliance/page.js
+++ src/app/services/quality-and-compliance/page.js
@@ -1,11 +1,11 @@
-// import QualityContent from "./QualityContent"; 
+import QualityContent from "./QualityContent"; // Import the client component
 
-// export const metadata = {
-//   title: "Quality & Compliance Services | GxP Audits & QMS",
-//   description: "Ensure continuous adherence to industry standards with our End-to-End Compliance Support, Regulatory Expertise, and Quality Management System solutions.",
+export const metadata = {
+  title: "Quality & Compliance Services | GxP Audits & QMS",
+  description: "Ensure continuous adherence to industry standards with our End-to-End Compliance Support, Regulatory Expertise, and Quality Management System solutions.",
  
-// };
+};
 
 export default function Page() {
-  return <h1>hello</h1>;
+  return <QualityContent />;
 }
